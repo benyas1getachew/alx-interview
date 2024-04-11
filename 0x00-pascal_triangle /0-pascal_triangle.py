306288efd65f6f5f3431f80fd4c664d96b64b4e7
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-04-12 01:50:52 +0530

    my commit

--- 0x00-pascal_triangle /0-pascal_triangle.py
+++ 0x00-pascal_triangle /0-pascal_triangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Pascal's Triangle Interview Challenge"""
+
+
 def pascal_triangle(n):
+    """Returns a list of lists containing each row of the pascal's triangle"""
     if n <= 0:
         return []
 
