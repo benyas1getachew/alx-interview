e97e0b64eb38d01c1f1e3b6bb33e9e9dfad95bad
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-05-21 22:05:05 +0530

    my commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" N Queens """
+""" N queens """
 import sys
+
 
 if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
@@ -16,25 +17,28 @@ if int(sys.argv[1]) < 4:
 
 n = int(sys.argv[1])
 
-def queen(n, i=0, a=[], b=[], c=[]):
+
+def queens(n, i=0, a=[], b=[], c=[]):
     """ find possible positions """
     if i < n:
         for j in range(n):
             if j not in a and i + j not in b and i - j not in c:
-                yield from queen(n, i + 1, a + [j], b + [i + j], c + [i - j])
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
         yield a
+
 
 def solve(n):
     """ solve """
     k = []
     i = 0
-    for solution in queen(n, 0):
+    for solution in queens(n, 0):
         for s in solution:
             k.append([i, s])
             i += 1
         print(k)
         k = []
         i = 0
-      
+
+
 solve(n)
