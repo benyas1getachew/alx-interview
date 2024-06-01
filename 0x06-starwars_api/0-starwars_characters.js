4c7de81cb4e66d5cb58d490a9bea5484ef5e1a20
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-06-01 21:44:42 +0530

    my commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,21 +4,33 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
+function fetchCharacterName(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      }
+    });
+  });
+}
+
+request(apiUrl, async (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
   const filmData = JSON.parse(body);
   const characters = filmData.characters;
-  characters.forEach(characterUrl => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error(charError);
-        return;
-      }
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-    });
-  });
+
+  try {
+    for (const characterUrl of characters) {
+      const characterName = await fetchCharacterName(characterUrl);
+      console.log(characterName);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
